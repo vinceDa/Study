f33b08a95d0fdf9cb7a250f8ffd71ea01f9fa62d
ohYoung <vincedavince@163.com>
2020-11-06 14:30:32 +0800

    add .gitignore file

---
+++ translate/src/arrays/Test.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.util.Arrays;
+
+/**
+ * @author ohYoung
+ * @date 2020/11/5 14:45
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int i = 3;
+        int j = 6;
+        System.out.println(i ^ 2);
+        // System.out.println(i<<=1);
+        System.out.println(j ^= i);
+    }
+
+}
