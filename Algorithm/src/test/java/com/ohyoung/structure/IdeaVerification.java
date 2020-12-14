1ce53a45c396d10dcede5c02ca9a86fe750476e1
ohYoung <vincedavince@163.com>
2020-12-14 09:19:19 +0800

    添加个测试类 想到啥就去测试确认一下~

---
+++ Algorithm/src/test/java/com/ohyoung/structure/IdeaVerification.java
@@ -0,0 +1,19 @@
+package com.ohyoung.structure;
+
+import org.junit.Test;
+
+import java.util.Vector;
+
+/**
+ * @author ohYoung
+ * @date 2020/12/8 11:23
+ */
+public class IdeaVerification {
+
+    @Test
+    public void hashMap() {
+        Vector vector = new Vector();
+
+    }
+
+}
