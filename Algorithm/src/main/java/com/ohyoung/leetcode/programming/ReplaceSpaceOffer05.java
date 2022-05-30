90eb32c866fbfff8835a5eb0a20d0d16b0fc2baf
vince <vinceDavince@163.com>
2022-05-30 23:20:32 +0800

    feat: 初始化需要做的算法题

---
+++ Algorithm/src/main/java/com/ohyoung/leetcode/programming/ReplaceSpaceOffer05.java
@@ -0,0 +1,27 @@
+package com.ohyoung.leetcode.programming;
+
+/**
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ *
+ * 示例 1：
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ *
+ * 限制：
+ * 0 <= s 的长度 <= 10000
+ *
+ * @author ohYoung
+ * @date 2022/5/30 23:12
+ */
+public class ReplaceSpaceOffer05 {
+
+    public static void main(String[] args) {
+        ReplaceSpaceOffer05 example = new ReplaceSpaceOffer05();
+        System.out.println("LengthOfLastWord58 We are happy.: " + example.replaceSpace("We are happy."));
+    }
+
+    public String replaceSpace(String s) {
+        return "";
+    }
+
+}
