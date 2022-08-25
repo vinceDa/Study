2c0995961d74e4c3d68139e986630913ce6bf9fb
ouyb01 <jz+Q1628027779>
2022-08-25 17:27:13 +0800

    -feat 实现单Reactor模式

--- Concurrent/src/main/java/com/ohyoung/core/programming/nio/reactor/EchoClient.java
+++ Concurrent/src/main/java/com/ohyoung/core/programming/nio/reactor/EchoClient.java
@@ -1,6 +1,19 @@
 package com.ohyoung.core.programming.nio.reactor;
 
+import lombok.Data;
+
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.StandardSocketOptions;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.time.LocalDate;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author ouyb01
@@ -9,7 +22,108 @@ import java.io.IOException;
 public class EchoClient {
 
     public void start() throws IOException {
+        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 18899));
+        socketChannel.configureBlocking(false);
+        socketChannel.setOption(StandardSocketOptions.TCP_NODELAY, true);
+        while (!socketChannel.finishConnect()) {
+
+        }
+        Processor processor = new Processor(socketChannel);
+        Commander commander = new Commander(processor);
+        new Thread(commander).start();
+        new Thread(processor).start();
 
     }
 
+    static class Commander implements Runnable {
+
+        Processor processor;
+
+        public Commander(Processor processor) {
+            this.processor = processor;
+        }
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) {
+
+                ByteBuffer buffer = processor.getSendBuffer();
+
+                Scanner scanner = new Scanner(System.in);
+                try {
+                    while (processor.hasData.get()) {
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("请输入发送内容: ");
+                if (scanner.hasNext()) {
+
+                    String next = scanner.next();
+                    buffer.put((LocalDate.now() + " >>" + next).getBytes());
+                    processor.hasData.set(true);
+                }
+
+            }
+        }
+    }
+
+    @Data
+    static class Processor implements Runnable {
+
+        ByteBuffer sendBuffer = ByteBuffer.allocate(1024);
+
+        ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+
+
+        protected AtomicBoolean hasData = new AtomicBoolean(false);
+
+        final Selector selector;
+
+        final SocketChannel socketChannel;
+
+        public Processor(SocketChannel socketChannel) throws IOException{
+            selector = Selector.open();
+            this.socketChannel = socketChannel;
+            socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!Thread.interrupted()) {
+                    selector.select();
+                    Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                    Iterator<SelectionKey> iterator = selectedKeys.iterator();
+                    while (iterator.hasNext()) {
+                        SelectionKey sk = iterator.next();
+                        if (sk.isWritable()) {
+                            if (hasData.get()) {
+                                SocketChannel channel = (SocketChannel) sk.channel();
+                                sendBuffer.flip();
+                                channel.write(sendBuffer);
+                                sendBuffer.clear();
+                                hasData.set(false);
+                            }
+                        } else if (sk.isReadable()) {
+                            SocketChannel channel = (SocketChannel) sk.channel();
+                            int length;
+                            while ((length = channel.read(readBuffer)) > 0) {
+                                readBuffer.flip();
+                                readBuffer.clear();
+                            }
+                        }
+                    }
+                    selectedKeys.clear();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new EchoClient().start();
+    }
 }
--- Concurrent/src/main/java/com/ohyoung/core/programming/nio/reactor/EchoServerReactor.java
+++ Concurrent/src/main/java/com/ohyoung/core/programming/nio/reactor/EchoServerReactor.java
@@ -6,7 +6,9 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * @author ouyb01
@@ -30,7 +32,35 @@ public class EchoServerReactor implements Runnable{
 
     @Override
     public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                // 限时阻塞查询
+                selector.select(1000);
+                Set<SelectionKey> selected = selector.selectedKeys();
+                if (Objects.isNull(selected) || selected.isEmpty()) {
+                    continue;
+                }
+                Iterator<SelectionKey> it = selected.iterator();
+                while (it.hasNext()) {
+                    // Reactor负责dispatch收到的事件
+                    SelectionKey sk = it.next();
+                    // 避免下次重复处理
+                    it.remove();
+                    dispatch(sk);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+
+    void dispatch(SelectionKey sk) {
+        Runnable handler = (Runnable) sk.attachment();
+        // 调用之前attach绑定到选择键的handler处理对象
+        if (Objects.nonNull(handler)) {
+            handler.run();
+        }
     }
 
     class AcceptorHandler implements Runnable {
@@ -48,5 +78,9 @@ public class EchoServerReactor implements Runnable{
             }
 
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Thread(new EchoServerReactor()).start();
     }
 }
